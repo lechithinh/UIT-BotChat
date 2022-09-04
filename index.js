e343f25338cf0959427dee61e026acf10ebc2bca
ngohiep-nood <hoanghiephai@gmail.com>
2022-09-04 20:08:06 +0700

    chang url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", function(req, res) {
+app.get("/calendar", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
